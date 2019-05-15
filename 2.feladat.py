3bc4b1f2ad8eea0b8da78ef5f4fa130a0ffd45ba
lendonat <47624008+lendonat@users.noreply.github.com>
2019-05-15 20:42:51 +0200

    Update 2.feladat.py

--- 2.feladat.py
+++ 2.feladat.py
@@ -1,17 +1,10 @@
-import numpy as np
 def alsoharomszog(matrix):
-    n=0
-    m=len(matrix[0])
-    for i in range(0,m-2):
-        if matrix[i][m-1]!=0:
-            return "nem also haromszog matrix"
-        for j in range(1,n):
-            n+=1
-            if n!=m:
-                if matrix[i][j]!=0:
-                    return "nem also haromszog matrix"
-    return "also haromszog matrix"
-x=alsoharomszog([[1,0,0,0],[1,1,0,0],[1,1,1,0],[1,1,1,1]])
+    for i in range(0, len(matrix)): 
+        for j in range(i + 1, len(matrix)): 
+            if(matrix[i][j] != 0):  
+                    return False
+    return True            
+x=alsoharomszog([[1,0,0,0],[1,1,0,0],[1,1,1,0],[1,1,1,1]]) #teszt
 print(x)
 
 
